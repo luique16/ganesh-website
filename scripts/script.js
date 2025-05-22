5761b0d8366a5c8ad94f7f820152a87a910064fb
Luís Henrique <luish.fa1515@gmail.com>
2025-05-22 00:30:52 -0300

    Página principal

---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+var language = "pt-BR";
+
+const response = await fetch("../data/data.json");
+const data = await response.json();
+
+let languageButton = document.querySelector("#language-button p");
+
+const names = {
+    "pt-BR": {
+        "language-buttom": "pt-BR",
+        "language-flag": "BR.svg",
+        "news-button": "Notícias",
+        "about-title": "Sobre o <span style='color: #5EDA4A'>Ganesh</span>",
+        "about-button": "Saiba mais",
+    },
+    "en-US": {
+        "language-buttom": "en-US",
+        "language-flag": "US.svg",
+        "news-button": "News",
+        "about-title": "About <span style='color: #5EDA4A'>Ganesh</span>",
+        "about-button": "Learn more",
+    }
+}
+
+function updateLanguage() {
+    languageButton.innerText = names[language]["language-buttom"];
+    document.querySelector("#language-button img").src = `assets/${names[language]["language-flag"]}`;
+    document.getElementById("news-button").innerText = names[language]["news-button"];
+    document.getElementById("presentation-text").innerText = data.informacoes[language.toLowerCase()].subtitulo;
+    document.getElementById("about-title").innerHTML = names[language]["about-title"];
+    document.getElementById("about-text").innerHTML = data.informacoes[language.toLowerCase()].sobre;
+    document.querySelector("#about-button span").innerHTML = names[language]["about-button"];
+}
+
+function swapLanguage() {
+    if(language === "pt-BR") {
+        language = "en-US";
+    } else {
+        language = "pt-BR";
+    }
+
+    updateLanguage();
+}
+
+document.getElementById("language-button").addEventListener("click", swapLanguage);
